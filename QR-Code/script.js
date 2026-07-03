0b8ac77dc1e962df4115ed88d808be2c322c53bd
MaheshMergu01 <maheshmergu14@gmail.com>
2026-07-03 19:15:42 +0530

    initial commit

---
+++ QR-Code/script.js
@@ -0,0 +1,10 @@
+const qrtext = document.getElementById("qr-text");
+const sizes = document.getElementById("sizes");
+const generateBtn = document.getElementById("generateBtn");
+const downloadBtn = document.getElementById("downloadBtn");
+
+const qrContainer = document.querySelector(".qr-body");
+
+generateBtn.addEventListener("click", e => {
+  e.preventDefault();
+});
