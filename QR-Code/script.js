5bd13a90718b1c8263327d132ba85648f57fd705
MaheshMergu01 <maheshmergu14@gmail.com>
2026-07-04 20:14:14 +0530

    Updated Changes

--- QR-Code/script.js
+++ QR-Code/script.js
@@ -5,6 +5,31 @@ const downloadBtn = document.getElementById("downloadBtn");
 
 const qrContainer = document.querySelector(".qr-body");
 
+let size = sizes.value;
 generateBtn.addEventListener("click", e => {
   e.preventDefault();
+  isEmptyInput();
 });
+
+sizes.addEventListener("change", e => {
+  size = e.target.value;
+  isEmptyInput();
+});
+
+function isEmptyInput() {
+  if (qrtext.value.length > 0) {
+    generateQRCode();
+  } else {
+    alert("Enter the Text or URL to generate your QR Code");
+  }
+}
+function generateQRCode() {
+  qrContainer.innerHTML = "";
+  new QRCode(qrContainer, {
+    text: qrtext.value,
+    height: size,
+    width: size,
+    colorLight: "#fff",
+    colorDark: "#000"
+  });
+}
